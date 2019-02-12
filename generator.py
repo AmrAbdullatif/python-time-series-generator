44b858fd0c88e81762a20712f8f4588375f321a5
AmrAbdullatif <31695791+AmrAbdullatif@users.noreply.github.com>
2019-02-12 16:02:23 +0100

    fix indices max_index + 1

--- generator.py
+++ generator.py
@@ -32,7 +32,7 @@ def generator(data, lookback, delay, min_index, max_index, shuffle=False, batch_
     i = min_index + lookback
     while 1:
         if shuffle:
-            rows = np.random.randint(min_index + lookback, max_index, size=batch_size)
+            rows = np.random.randint(min_index + lookback, max_index + 1, size=batch_size)
         else:
             if i + batch_size >= max_index:
                 i = min_index + lookback
